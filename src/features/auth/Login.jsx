d39268b5153f5126f57396ef4c387f9fac63513c
Yefersn15 <yefersonandres225@gmail.com>
2026-08-23 14:17:07 -0500

    Cambios en validaciones

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -102,7 +102,7 @@ const Login = () => {
       position: 'relative',
       overflow: 'hidden',
     }}>
-      {cargando && <LoadingScreen mensaje="Preparando panel de administrador..." />}
+      {cargando && <LoadingScreen mensaje="Logeado correctamente: Preparando panel de administrador..." />}
 
       <Box sx={{
         position: 'absolute', top: 0, left: 0, right: 0, height: 4,
