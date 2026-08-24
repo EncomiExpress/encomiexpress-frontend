bee23a3814c912f4ad3b1e29ac5d28d8d2314efd
Yefersn15 <yefersonandres225@gmail.com>
2026-08-23 19:43:24 -0500

    cambio en mensaje de loggin

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -121,7 +121,7 @@ const Login = () => {
       position: 'relative',
       overflow: 'hidden',
     }}>
-      {cargando && <LoadingScreen mensaje="Logeado correctamente: Preparando panel de administrador..." />}
+      {cargando && <LoadingScreen mensaje={<>Logeado correctamente:<br />Preparando panel de administrador...</>} />}
 
       <Box sx={{
         position: 'absolute', top: 0, left: 0, right: 0, height: 4,
